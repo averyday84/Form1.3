9abb637ac156d636e13feb613e3c909ab0a0cb50
averyday84 <142625338+averyday84@users.noreply.github.com>
2023-08-25 23:35:26 -0700

    attached files

---
+++ form-viewer.php
@@ -0,0 +1,10 @@
+<?php
+
+  $FullName = htmlspecialchars($_POST['FullName']);
+  $Email  = htmlspecialchars($_POST['Email']);
+  $Phone  = htmlspecialchars($_POST['Phone']);
+  $contact  = htmlspecialchars($_POST['contact']);
+  $state  = htmlspecialchars($_POST['state']);
+  $Comments  = htmlspecialchars($_POST['Comments']);
+
+  ?>
